05dfffee6a988a86b762e5a2769588ded44d1ae1
colinjlacy <colin@webcake.co>
2016-02-11 19:54:54 -0600

    [working-toggle] revises data service to set up a working toggle

--- app/pages/page1/page1.js
+++ app/pages/page1/page1.js
@@ -5,65 +5,52 @@ import 'rxjs/add/operator/map';
 
 
 @Page({
-  templateUrl: 'build/pages/page1/page1.html'
+	templateUrl: 'build/pages/page1/page1.html'
 })
 export class Page1 {
-  constructor( fb: FormBuilder, http: Http) {
+	public DataItem;
+	constructor( fb: FormBuilder, http: Http) {
 
-    this.http = http;
+		this.http = http;
 
-    this.DataItem = [];
+		this.dataForm = fb.group({
+			name_private: ["", Validators.required]
+		});
 
-    this.initDataItem();
+	}
 
-    this.dataForm = fb.group({
-      name_private: ["", Validators.required]
-    });
+	onSave(form) {
+		event.preventDefault();
+	}
 
-  }
+	//stringToBoolean(string) {
+	//  switch (string.toLowerCase().trim()) {
+	//    case "true":
+	//    case "yes":
+	//    case "1":
+	//    case true:
+	//      return true;
+	//    case "false":
+	//    case "no":
+	//    case "0":
+	//    case null:
+	//      return false;
+	//    default:
+	//      return Boolean(string);
+	//  }
+	//}
 
-
-  onSave(form)
-  {
-
-    console.log(this.DataItem.published);
-
-    event.preventDefault();
-  }
-
-  stringToBoolean(string) {
-    switch (string.toLowerCase().trim()) {
-      case "true":
-      case "yes":
-      case "1":
-        return true;
-      case "false":
-      case "no":
-      case "0":
-      case null:
-        return false;
-      default:
-        return Boolean(string);
-    }
-  }
-
-  initDataItem()
-  {
-
-    this.http.get(window.location.protocol + '//' + window.location.hostname+ '/TestToggle/get.php?q=test')
-
-        .map((res: Response) => res.json())
-
-  .subscribe(
-      data => {
-
-    this.DataItem.published = this.stringToBoolean(data.data[0].published);
-
-  },
-    err => console.error(err)
-  );
-
-  }
+	onPageWillEnter() {
+		this.initDataItem();
+	}
+	initDataItem() {
+		this.http.get('./build/js/test.json')
+			.map((res) => res.json())
+			.subscribe(
+				data => this.DataItem = data,
+				err => console.error(err)
+			);
+	}
 
 
 }
